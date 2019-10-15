a4ff3e7a35c1bcb5ad7efe61d46a558c82e31f32
Masa <ishiimasataka@yahoo.fr>
2019-10-15 14:26:22 +0200

    Add main and CMakeLists.txt for add lib curl

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * Filename : main.c
+ *
+ * Made by : Masataka ISHII
+ *
+ * Description : main function of scraper
+ */
+#include<stdio.h>
+
+
+int main(int argc, char **argv) {
+    printf("toto\n");
+    return 0;
+}
+
