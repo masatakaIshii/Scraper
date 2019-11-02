2734cd0634ffc302d7240e843fe651572b7ae077
Masa <ishiimasataka@yahoo.fr>
2019-11-02 01:52:39 +0100

    clean resource.c

--- sources/resource.c
+++ sources/resource.c
@@ -4,62 +4,84 @@
 
 #include "resource.h"
 
+static void initFieldsResource(Resource *pResource, int depth, int maxDepth) {
+    pResource->depth = depth;
+    pResource->maxDepth = maxDepth;
+
+    pResource->isCreatedDate = 0;
+    pResource->createdDate = NULL;
+
+    pResource->isDirResourcePath = 0;
+    pResource->dirResourcePath = NULL;
+
+    pResource->isOutputPath = 0;
+    pResource->outputPath = NULL;
+
+    pResource->isType = 0;
+    pResource->type = NULL;
+
+    pResource->isRequest = 0;
+}
+
 Resource *initResource(const char *url, int depth, int maxDepth) {
     Resource *pResource = NULL;
-    if (maxDepth < depth) {
+    if (maxDepth >= 0 && depth >= 0 && maxDepth >= depth) {
         pResource = malloc(sizeof(Resource));
         if (pResource == NULL) {
             return NULL;
         }
-        pResource->depth = depth;
-        pResource->maxDepth = maxDepth;
+        initFieldsResource(pResource, depth, maxDepth);
         pResource->pRequest = initRequest(url);
         if (pResource->pRequest == NULL) {
-            free(pResource->createdDate);
             free(pResource);
             return NULL;
         }
-        pResource->createdDate = NULL;
-        pResource->dirResourcePath = NULL;
-        pResource->outputPath = NULL;
-        pResource->type = NULL;
+        pResource->isRequest = 1;
     }
 
     return pResource;
 }
 
-static int setDirAndOutputPath(Resource *pResource, const char *dirResourcePath) {
+static int setOutputPath(Resource *pResource) {
     UrlHelper *pUrlHelper = pResource->pRequest->pUrlHelper;
     char *fileNameWithExt = NULL;
 
-    pResource->dirResourcePath = strMallocCpy(dirResourcePath, strlen(dirResourcePath));
-    if (pResource->dirResourcePath == NULL) {
-        printf("Problem malloc directory path of resource");
-        return -1;
-    }
-
     if (pUrlHelper->isExtFile == 1) {
         pResource->outputPath = strMallocCat(pResource->dirResourcePath, pUrlHelper->fileName);
-        if (pResource->outputPath == NULL) {
-            errorQuit("Problem malloc output file path in resource\n");
-            return -1;
-        }
+        verifyPointer(pResource->outputPath, "Problem malloc output file path in resource\n");
     } else {
-        if (getExtFileByMimeType(pUrlHelper)) {
+        if (getExtFileByMimeType(pUrlHelper)) { // fetch extension file by mime type search in conditions and list extFile / mimeType
             fileNameWithExt = strMallocCat(pUrlHelper->fileName, pUrlHelper->extFile);
+            verifyPointer(fileNameWithExt, "Problem malloc string fileNameWithExt path in resource\n");
+
             pResource->outputPath = strMallocCat(pResource->dirResourcePath, fileNameWithExt);
+            verifyPointer(pResource->outputPath, "Problem malloc outputPath in resource\n");
+            free(fileNameWithExt);
         } else {
             return 1;
         }
     }
-
+    pResource->isOutputPath = 1;
     return 0;
 }
 
-int createFileResource(Resource *pResource, const char *dirResourcePath) {
-    int result = setDirAndOutputPath(pResource, dirResourcePath);
-    if (result != 0) {
+static int setDirAndOutputPath(Resource *pResource, const char *dirResourcePath) {
+    int result = 0;
+    pResource->dirResourcePath = strMallocCpy(dirResourcePath, strlen(dirResourcePath));
+    verifyPointer(pResource->dirResourcePath, "Problem malloc directory path of resource");
 
+    pResource->isDirResourcePath = 1;
+
+    result = setOutputPath(pResource);
+
+    return result;
+}
+
+int createFileResource(Resource *pResource, const char *dirResourcePath) {
+    int result;
+
+    result = setDirAndOutputPath(pResource, dirResourcePath);
+    if (result != 0) {
         fprintf(stderr, "Don't found file extention of resource with url '%s'", pResource->pRequest->pUrlHelper->url);
         return -1;
     }
@@ -70,10 +92,16 @@ int createFileResource(Resource *pResource, const char *dirResourcePath) {
     }
 
     pResource->createdDate = getCurrentTime();
+    verifyPointer(pResource->createdDate, "Problem get current time in createFileResource\n");
+    pResource->isCreatedDate = 1;
 
     return 0;
 }
 
 void addResourceInfoInFile(Resource *pResource, const char *resourcesFile) {
+
+}
+
+void destroyResource(Resource *pResource) {
 
 }
