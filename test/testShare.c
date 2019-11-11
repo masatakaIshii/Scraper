e8e944af42a859e8b101a65468a93b8aba6b637a
Masa <ishiimasataka@yahoo.fr>
2019-11-11 19:16:18 +0100

    Add function to share to few tests, to put stderr in file

--- test/testShare.c
+++ test/testShare.c
@@ -22,3 +22,17 @@ int cleanManageStderr() {
     unlink(MANAGE_ERR_FILE);
     return 0;
 }
+
+int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
+    int rv = remove(fpath);
+
+    if (rv)
+        perror(fpath);
+
+    return rv;
+}
+
+int rmrf(char *path) {
+    nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
+    return rmdir(path);
+}
--- test/testShare.h
+++ test/testShare.h
@@ -6,11 +6,16 @@
 #define SCRAPER_TESTSHARE_H
 
 #include <stdio.h>
+#include <ftw.h>
 
 #define MANAGE_ERR_FILE "errors.txt"
 
 int initManageStderr();
 
 int cleanManageStderr();
+
+int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf);
+
+int rmrf(char *path);
 
 #endif //SCRAPER_TESTSHARE_H
