2d16b20746a5fbd07fcd9dc99d7324bd1fe9cee2
Masa <ishiimasataka@yahoo.fr>
2019-11-13 00:18:53 +0100

    urlSearcher update

--- sources/functions/urlSearcher.c
+++ sources/functions/urlSearcher.c
@@ -2,4 +2,17 @@
 // Created by masat on 12/11/2019.
 //
 
-#include "urlSearcher.h"
+#include "../headers/urlSearcher.h"
+
+/**
+ * Function to get all http url in page that each url is unique
+ * @param UrlNoRes : the url with only http syntax and domain name like "https://www.google.com"
+ * @param page : the page that correspond to the
+ * @param count : number of url
+ * @return allUrls : array of url that fetch in page
+ */
+char **getAllUrlsInPage(const char *UrlNoRes, const char *contentType, const char *page, int *count) {
+    char **allUrls = NULL;
+
+    return NULL;
+}
--- sources/headers/urlSearcher.h
+++ sources/headers/urlSearcher.h
@@ -5,4 +5,8 @@
 #ifndef SCRAPER_URLSEARCHER_H
 #define SCRAPER_URLSEARCHER_H
 
+#include <stdio.h>
+
+char **getAllUrlsInPage(const char *UrlNoRes, const char *contentType, const char *page, int *count);
+
 #endif //SCRAPER_URLSEARCHER_H
